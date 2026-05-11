ddcb9abda3d90af058eb4c83ee8967d51bfa1d82
Allen Jackson <spacecatprime@gmail.com>
2026-05-11 18:49:37 -0500

    initial app framework for fbsim

---
+++ FootballSim_console/Program.cs
@@ -0,0 +1,10 @@
+﻿// next step is to pull in the library
+
+using FootballSim_lib;
+
+System.Console.WriteLine("Starting...");
+
+FootballSim_lib.Class1.debug();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ FootballSim_lib/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace FootballSim_lib;
+
+public class Class1
+{
+    public static void debug()
+    {
+        System.Console.WriteLine("hello dude");
+    }
+
+}
